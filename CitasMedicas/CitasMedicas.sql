a6ed8add89a20f5cadafb83087e53461d3da7e80
RonnyCabrera <ronny-38@hotmail.com>
2018-05-28 23:58:39 -0400

    Correcion de Error Usuario-Perfil.

--- CitasMedicas/CitasMedicas.sql
+++ CitasMedicas/CitasMedicas.sql
@@ -363,34 +363,23 @@ go
 /*==============================================================*/
 /* Table: ADMINISTRADOR                                         */
 /*==============================================================*/
-create table ADMINISTRADOR (
-   ID_ADMINISTRADOR     int                  identity,
-   ID_CLINICA           int                  null,
-   ID_PERFIL            int                  null,
-   NOMBRES_ADMINISTRADOR varchar(50)          not null,
-   APELLIDOS_ADMINISTRADOR varchar(50)          not null,
-   CEDULA_ADMINISTRADOR varchar(10)          not null,
-   CORREO_ADMINISTRADOR varchar(30)          not null,
-   DIRECCION_ADMINISTRADOR varchar(50)          not null,
-   constraint PK_ADMINISTRADOR primary key nonclustered (ID_ADMINISTRADOR)
-)
-go
-
-/*==============================================================*/
-/* Index: RELATIONSHIP_14_FK                                    */
-/*==============================================================*/
-create index RELATIONSHIP_14_FK on ADMINISTRADOR (
-ID_PERFIL ASC
-)
-go
-
-/*==============================================================*/
-/* Index: RELATIONSHIP_15_FK                                    */
-/*==============================================================*/
-create index RELATIONSHIP_15_FK on ADMINISTRADOR (
-ID_CLINICA ASC
-)
-go
+CREATE TABLE [dbo].[ADMINISTRADOR]
+(
+[ID_ADMINISTRADOR] [int] NOT NULL IDENTITY(1, 1),
+[ID_USUARIO] [int] NULL,
+[NOMBRES_ADMINISTRADOR] [varchar] (50) COLLATE Modern_Spanish_CI_AS NOT NULL,
+[APELLIDOS_ADMINISTRADOR] [varchar] (50) COLLATE Modern_Spanish_CI_AS NOT NULL,
+[CEDULA_ADMINISTRADOR] [varchar] (10) COLLATE Modern_Spanish_CI_AS NOT NULL,
+[CORREO_ADMINISTRADOR] [varchar] (30) COLLATE Modern_Spanish_CI_AS NOT NULL,
+[DIRECCION_ADMINISTRADOR] [varchar] (50) COLLATE Modern_Spanish_CI_AS NOT NULL
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[ADMINISTRADOR] ADD CONSTRAINT [PK_ADMINISTRADOR] PRIMARY KEY NONCLUSTERED  ([ID_ADMINISTRADOR]) ON [PRIMARY]
+GO
+CREATE NONCLUSTERED INDEX [RELATIONSHIP_8_FK] ON [dbo].[ADMINISTRADOR] ([ID_USUARIO]) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[ADMINISTRADOR] ADD CONSTRAINT [FK_ADMINIST_RELATIONS_USUARIO] FOREIGN KEY ([ID_USUARIO]) REFERENCES [dbo].[USUARIO] ([ID_USUARIO])
+GO
 
 /*==============================================================*/
 /* Table: CITA                                                  */
@@ -433,24 +422,17 @@ go
 /*==============================================================*/
 /* Table: CLINICA                                               */
 /*==============================================================*/
-create table CLINICA (
-   ID_CLINICA           int                  identity,
-   ID_ADMINISTRADOR     int                  null,
-   NOMBRE_CLINICA       varchar(20)          not null,
-   TELEFONO_CLINICA     varchar(10)          not null,
-   CORREO_CLINICA       varchar(30)          not null,
-   DIRECCION_CLINICA    varchar(50)          null,
-   constraint PK_CLINICA primary key nonclustered (ID_CLINICA)
-)
-go
-
-/*==============================================================*/
-/* Index: RELATIONSHIP_16_FK                                    */
-/*==============================================================*/
-create index RELATIONSHIP_16_FK on CLINICA (
-ID_ADMINISTRADOR ASC
-)
-go
+CREATE TABLE [dbo].[CLINICA]
+(
+[ID_CLINICA] [int] NOT NULL IDENTITY(1, 1),
+[NOMBRE_CLINICA] [varchar] (20) COLLATE Modern_Spanish_CI_AS NOT NULL,
+[TELEFONO_CLINICA] [varchar] (10) COLLATE Modern_Spanish_CI_AS NOT NULL,
+[CORREO_CLINICA] [varchar] (30) COLLATE Modern_Spanish_CI_AS NOT NULL,
+[DIRECCION_CLINICA] [varchar] (50) COLLATE Modern_Spanish_CI_AS NULL
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[CLINICA] ADD CONSTRAINT [PK_CLINICA] PRIMARY KEY NONCLUSTERED  ([ID_CLINICA]) ON [PRIMARY]
+GO
 
 /*==============================================================*/
 /* Table: CONSULTA                                              */
@@ -477,21 +459,19 @@ go
 /*==============================================================*/
 /* Table: DIA                                                   */
 /*==============================================================*/
-create table DIA (
-   ID_DIA               int                  identity,
-   ID_MEDICO            int                  null,
-   NOMBRE_DIA           varchar(10)          not null,
-   constraint PK_DIA primary key nonclustered (ID_DIA)
-)
-go
-
-/*==============================================================*/
-/* Index: RELATIONSHIP_18_FK                                    */
-/*==============================================================*/
-create index RELATIONSHIP_18_FK on DIA (
-ID_MEDICO ASC
-)
-go
+CREATE TABLE [dbo].[DIA]
+(
+[ID_DIA] [int] NOT NULL IDENTITY(1, 1),
+[ID_MEDICO] [int] NULL,
+[NOMBRE_DIA] [varchar] (10) COLLATE Modern_Spanish_CI_AS NOT NULL
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[DIA] ADD CONSTRAINT [PK_DIA] PRIMARY KEY NONCLUSTERED  ([ID_DIA]) ON [PRIMARY]
+GO
+CREATE NONCLUSTERED INDEX [RELATIONSHIP_13_FK] ON [dbo].[DIA] ([ID_MEDICO]) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[DIA] ADD CONSTRAINT [FK_DIA_RELATIONS_MEDICO] FOREIGN KEY ([ID_MEDICO]) REFERENCES [dbo].[MEDICO] ([ID_MEDICO])
+GO
 
 /*==============================================================*/
 /* Table: ESPECIALIDAD                                          */
@@ -507,55 +487,47 @@ go
 /*==============================================================*/
 /* Table: HORA                                                  */
 /*==============================================================*/
-create table HORA (
-   ID_HORA              int                  identity,
-   ID_DIA               int                  null,
-   HORA_INICIAL         datetime             not null,
-   HORA_FINAL           datetime             not null,
-   constraint PK_HORA primary key nonclustered (ID_HORA)
-)
-go
-
-/*==============================================================*/
-/* Index: RELATIONSHIP_17_FK                                    */
-/*==============================================================*/
-create index RELATIONSHIP_17_FK on HORA (
-ID_DIA ASC
-)
-go
+CREATE TABLE [dbo].[HORA]
+(
+[ID_HORA] [int] NOT NULL IDENTITY(1, 1),
+[ID_DIA] [int] NULL,
+[HORA_INICIAL] [datetime] NOT NULL,
+[HORA_FINAL] [datetime] NOT NULL
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[HORA] ADD CONSTRAINT [PK_HORA] PRIMARY KEY NONCLUSTERED  ([ID_HORA]) ON [PRIMARY]
+GO
+CREATE NONCLUSTERED INDEX [RELATIONSHIP_12_FK] ON [dbo].[HORA] ([ID_DIA]) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[HORA] ADD CONSTRAINT [FK_HORA_RELATIONS_DIA] FOREIGN KEY ([ID_DIA]) REFERENCES [dbo].[DIA] ([ID_DIA])
+GO
 
 /*==============================================================*/
 /* Table: MEDICO                                                */
 /*==============================================================*/
-create table MEDICO (
-   ID_MEDICO            int                  identity,
-   ID_PERFIL            int                  null,
-   ID_ESPECIALIDAD      int                  null,
-   NOMBRES_MEDICO       varchar(50)          not null,
-   APELLIDOS_MEDICO     varchar(50)          not null,
-   CEDULA_MEDICO        varchar(10)          not null,
-   TELEFONO_MEDICO      varchar(10)          not null,
-   CORREO_MEDICO        varchar(30)          not null,
-   DIRECCION_MEDICO     varchar(50)          not null,
-   constraint PK_MEDICO primary key nonclustered (ID_MEDICO)
-)
-go
-
-/*==============================================================*/
-/* Index: RELATIONSHIP_1_FK                                     */
-/*==============================================================*/
-create index RELATIONSHIP_1_FK on MEDICO (
-ID_ESPECIALIDAD ASC
-)
-go
-
-/*==============================================================*/
-/* Index: RELATIONSHIP_10_FK                                    */
-/*==============================================================*/
-create index RELATIONSHIP_10_FK on MEDICO (
-ID_PERFIL ASC
-)
-go
+CREATE TABLE [dbo].[MEDICO]
+(
+[ID_MEDICO] [int] NOT NULL IDENTITY(1, 1),
+[ID_USUARIO] [int] NULL,
+[ID_ESPECIALIDAD] [int] NULL,
+[NOMBRES_MEDICO] [varchar] (50) COLLATE Modern_Spanish_CI_AS NOT NULL,
+[APELLIDOS_MEDICO] [varchar] (50) COLLATE Modern_Spanish_CI_AS NOT NULL,
+[CEDULA_MEDICO] [varchar] (10) COLLATE Modern_Spanish_CI_AS NOT NULL,
+[TELEFONO_MEDICO] [varchar] (10) COLLATE Modern_Spanish_CI_AS NOT NULL,
+[CORREO_MEDICO] [varchar] (30) COLLATE Modern_Spanish_CI_AS NOT NULL,
+[DIRECCION_MEDICO] [varchar] (50) COLLATE Modern_Spanish_CI_AS NOT NULL
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[MEDICO] ADD CONSTRAINT [PK_MEDICO] PRIMARY KEY NONCLUSTERED  ([ID_MEDICO]) ON [PRIMARY]
+GO
+CREATE NONCLUSTERED INDEX [RELATIONSHIP_1_FK] ON [dbo].[MEDICO] ([ID_ESPECIALIDAD]) ON [PRIMARY]
+GO
+CREATE NONCLUSTERED INDEX [RELATIONSHIP_10_FK] ON [dbo].[MEDICO] ([ID_USUARIO]) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[MEDICO] ADD CONSTRAINT [FK_MEDICO_RELATIONS_ESPECIAL] FOREIGN KEY ([ID_ESPECIALIDAD]) REFERENCES [dbo].[ESPECIALIDAD] ([ID_ESPECIALIDAD])
+GO
+ALTER TABLE [dbo].[MEDICO] ADD CONSTRAINT [FK_MEDICO_RELATIONS_USUARIO] FOREIGN KEY ([ID_USUARIO]) REFERENCES [dbo].[USUARIO] ([ID_USUARIO])
+GO
 
 /*==============================================================*/
 /* Table: PACIENTE                                              */
@@ -575,106 +547,58 @@ go
 /*==============================================================*/
 /* Table: PERFIL                                                */
 /*==============================================================*/
-create table PERFIL (
-   ID_PERFIL            int                  identity,
-   ID_RECEPCIONISTA     int                  null,
-   ID_MEDICO            int                  null,
-   ID_USUARIO           int                  null,
-   ID_ADMINISTRADOR     int                  null,
-   FOTO                 image                not null,
-   COLOR                varchar(10)          not null,
-   LETRA                varchar(30)          not null,
-   constraint PK_PERFIL primary key nonclustered (ID_PERFIL)
-)
-go
-
-/*==============================================================*/
-/* Index: RELATIONSHIP_8_FK                                     */
-/*==============================================================*/
-create index RELATIONSHIP_8_FK on PERFIL (
-ID_USUARIO ASC
-)
-go
-
-/*==============================================================*/
-/* Index: RELATIONSHIP_9_FK                                     */
-/*==============================================================*/
-create index RELATIONSHIP_9_FK on PERFIL (
-ID_MEDICO ASC
-)
-go
-
-/*==============================================================*/
-/* Index: RELATIONSHIP_11_FK                                    */
-/*==============================================================*/
-create index RELATIONSHIP_11_FK on PERFIL (
-ID_RECEPCIONISTA ASC
-)
-go
-
-/*==============================================================*/
-/* Index: RELATIONSHIP_13_FK                                    */
-/*==============================================================*/
-create index RELATIONSHIP_13_FK on PERFIL (
-ID_ADMINISTRADOR ASC
-)
-go
+CREATE TABLE [dbo].[PERFIL]
+(
+[ID_PERFIL] [int] NOT NULL IDENTITY(1, 1),
+[ID_USUARIO] [int] NULL,
+[FOTO] [image] NOT NULL,
+[COLOR] [varchar] (10) COLLATE Modern_Spanish_CI_AS NOT NULL,
+[LETRA] [varchar] (30) COLLATE Modern_Spanish_CI_AS NOT NULL
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[PERFIL] ADD CONSTRAINT [PK_PERFIL] PRIMARY KEY NONCLUSTERED  ([ID_PERFIL]) ON [PRIMARY]
+GO
+CREATE NONCLUSTERED INDEX [RELATIONSHIP_11_FK] ON [dbo].[PERFIL] ([ID_USUARIO]) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[PERFIL] ADD CONSTRAINT [FK_PERFIL_RELATIONS_USUARIO] FOREIGN KEY ([ID_USUARIO]) REFERENCES [dbo].[USUARIO] ([ID_USUARIO])
+GO
 
 /*==============================================================*/
 /* Table: RECEPCIONISTA                                         */
 /*==============================================================*/
-create table RECEPCIONISTA (
-   ID_RECEPCIONISTA     int                  identity,
-   ID_PERFIL            int                  null,
-   NOMBRES_RECEPCINISTA varchar(50)          not null,
-   APELLIDOS_RECEPCIONISTA varchar(50)          not null,
-   CEDULA_RECEPCIONISTA varchar(10)          not null,
-   TELEFONO_RECEPCIONISTA varchar(10)          not null,
-   CORREO_RECEPCIONISTA varchar(30)          not null,
-   DIRECCION_RECEPCIONISTA varchar(50)          not null,
-   constraint PK_RECEPCIONISTA primary key nonclustered (ID_RECEPCIONISTA)
-)
-go
-
-/*==============================================================*/
-/* Index: RELATIONSHIP_12_FK                                    */
-/*==============================================================*/
-create index RELATIONSHIP_12_FK on RECEPCIONISTA (
-ID_PERFIL ASC
-)
-go
+CREATE TABLE [dbo].[RECEPCIONISTA]
+(
+[ID_RECEPCIONISTA] [int] NOT NULL IDENTITY(1, 1),
+[ID_USUARIO] [int] NULL,
+[NOMBRES_RECEPCINISTA] [varchar] (50) COLLATE Modern_Spanish_CI_AS NOT NULL,
+[APELLIDOS_RECEPCIONISTA] [varchar] (50) COLLATE Modern_Spanish_CI_AS NOT NULL,
+[CEDULA_RECEPCIONISTA] [varchar] (10) COLLATE Modern_Spanish_CI_AS NOT NULL,
+[TELEFONO_RECEPCIONISTA] [varchar] (10) COLLATE Modern_Spanish_CI_AS NOT NULL,
+[CORREO_RECEPCIONISTA] [varchar] (30) COLLATE Modern_Spanish_CI_AS NOT NULL,
+[DIRECCION_RECEPCIONISTA] [varchar] (50) COLLATE Modern_Spanish_CI_AS NOT NULL
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[RECEPCIONISTA] ADD CONSTRAINT [PK_RECEPCIONISTA] PRIMARY KEY NONCLUSTERED  ([ID_RECEPCIONISTA]) ON [PRIMARY]
+GO
+CREATE NONCLUSTERED INDEX [RELATIONSHIP_9_FK] ON [dbo].[RECEPCIONISTA] ([ID_USUARIO]) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[RECEPCIONISTA] ADD CONSTRAINT [FK_RECEPCIO_RELATIONS_USUARIO] FOREIGN KEY ([ID_USUARIO]) REFERENCES [dbo].[USUARIO] ([ID_USUARIO])
+GO
 
 /*==============================================================*/
 /* Table: USUARIO                                               */
 /*==============================================================*/
-create table USUARIO (
-   ID_USUARIO           int                  identity,
-   ID_PERFIL            int                  null,
-   NOMBRE_USUARIO       varchar(20)          not null,
-   CONTRASENA_USUARIO   varchar(10)          not null,
-   ROL_USUARIO          varchar(15)          not null,
-   ESTADO_USUARIO       bit                  not null,
-   constraint PK_USUARIO primary key nonclustered (ID_USUARIO)
-)
-go
-
-/*==============================================================*/
-/* Index: RELATIONSHIP_7_FK                                     */
-/*==============================================================*/
-create index RELATIONSHIP_7_FK on USUARIO (
-ID_PERFIL ASC
-)
-go
-
-alter table ADMINISTRADOR
-   add constraint FK_ADMINIST_RELATIONS_PERFIL foreign key (ID_PERFIL)
-      references PERFIL (ID_PERFIL)
-go
-
-alter table ADMINISTRADOR
-   add constraint FK_ADMINIST_RELATIONS_CLINICA foreign key (ID_CLINICA)
-      references CLINICA (ID_CLINICA)
-go
+CREATE TABLE [dbo].[USUARIO]
+(
+[ID_USUARIO] [int] NOT NULL IDENTITY(1, 1),
+[NOMBRE_USUARIO] [varchar] (20) COLLATE Modern_Spanish_CI_AS NOT NULL,
+[CONTRASENA_USUARIO] [varchar] (10) COLLATE Modern_Spanish_CI_AS NOT NULL,
+[ROL_USUARIO] [varchar] (15) COLLATE Modern_Spanish_CI_AS NOT NULL,
+[ESTADO_USUARIO] [bit] NOT NULL
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[USUARIO] ADD CONSTRAINT [PK_USUARIO] PRIMARY KEY NONCLUSTERED  ([ID_USUARIO]) ON [PRIMARY]
+GO
 
 alter table CITA
    add constraint FK_CITA_RELATIONS_MEDICO foreign key (ID_MEDICO)
@@ -691,63 +615,7 @@ alter table CITA
       references RECEPCIONISTA (ID_RECEPCIONISTA)
 go
 
-alter table CLINICA
-   add constraint FK_CLINICA_RELATIONS_ADMINIST foreign key (ID_ADMINISTRADOR)
-      references ADMINISTRADOR (ID_ADMINISTRADOR)
-go
-
 alter table CONSULTA
    add constraint FK_CONSULTA_RELATIONS_CITA foreign key (ID_CITA)
       references CITA (ID_CITA)
 go
-
-alter table DIA
-   add constraint FK_DIA_RELATIONS_MEDICO foreign key (ID_MEDICO)
-      references MEDICO (ID_MEDICO)
-go
-
-alter table HORA
-   add constraint FK_HORA_RELATIONS_DIA foreign key (ID_DIA)
-      references DIA (ID_DIA)
-go
-
-alter table MEDICO
-   add constraint FK_MEDICO_RELATIONS_ESPECIAL foreign key (ID_ESPECIALIDAD)
-      references ESPECIALIDAD (ID_ESPECIALIDAD)
-go
-
-alter table MEDICO
-   add constraint FK_MEDICO_RELATIONS_PERFIL foreign key (ID_PERFIL)
-      references PERFIL (ID_PERFIL)
-go
-
-alter table PERFIL
-   add constraint FK_PERFIL_RELATIONS_RECEPCIO foreign key (ID_RECEPCIONISTA)
-      references RECEPCIONISTA (ID_RECEPCIONISTA)
-go
-
-alter table PERFIL
-   add constraint FK_PERFIL_RELATIONS_ADMINIST foreign key (ID_ADMINISTRADOR)
-      references ADMINISTRADOR (ID_ADMINISTRADOR)
-go
-
-alter table PERFIL
-   add constraint FK_PERFIL_RELATIONS_USUARIO foreign key (ID_USUARIO)
-      references USUARIO (ID_USUARIO)
-go
-
-alter table PERFIL
-   add constraint FK_PERFIL_RELATIONS_MEDICO foreign key (ID_MEDICO)
-      references MEDICO (ID_MEDICO)
-go
-
-alter table RECEPCIONISTA
-   add constraint FK_RECEPCIO_RELATIONS_PERFIL foreign key (ID_PERFIL)
-      references PERFIL (ID_PERFIL)
-go
-
-alter table USUARIO
-   add constraint FK_USUARIO_RELATIONS_PERFIL foreign key (ID_PERFIL)
-      references PERFIL (ID_PERFIL)
-go
-
